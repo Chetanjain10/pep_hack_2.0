5cbaf7cef805f20a5a0316693e9413d0400dddb4
Chetanjain10 <chetanjainsrs@gmail.com>
2021-06-24 21:59:48 +0530

    functionalities

--- script.js
+++ script.js
@@ -5,12 +5,65 @@ let history = document.querySelector(".history-option");
 let trashVisible=true;
 let redeemVisible=false;
 let historyVisible=false;
+let hists=0;
+
+let historyData = [];
+if(trashVisible==true){
+    let modal = `<input type="text" id="text1"></input>
+        <input type="text" id="text2"></input>
+        <button class="add-btn">Add</button>`
+        MC.innerHTML=modal;
+        
+        let addbtn = document.querySelector(".add-btn");
+        addbtn.addEventListener("click",function(){
+            let dataVal = document.getElementById("text1").value;
+            let pointsVal = document.getElementById("text2").value;
+            if(dataVal==""){
+                alert("Date is Empty!!!");
+            }else if(pointsVal==""){
+                alert("Weight is EMPTY!!!");
+            }else{
+            let arr = [dataVal,pointsVal];
+            historyData[hists]=arr;
+            for(let i=0;i<historyData.length;i++){
+                console.log(historyData[i]);
+            }
+            hists++;
+            document.getElementById("text1").value = "";
+            document.getElementById("text2").value = "";
+        }
+        });
+        trashVisible=true;
+        redeemVisible=false;
+        historyVisible=false;
+}
+
 trash.addEventListener("click",function(){
     if(trashVisible==false){
-        let modal = `<div class="modal">
-        <h1>hello</h1>
-    </div>`
+
+        let modal = `<input type="text" id="text1"></input>
+        <input type="text" id="text2"></input>
+        <button class="add-btn">Add</button>`
         MC.innerHTML=modal;
+        let addbtn = document.querySelector(".add-btn");
+        addbtn.addEventListener("click",function(){
+            let dataVal = document.getElementById("text1").value;
+            let pointsVal = document.getElementById("text2").value;
+            if(dataVal==""){
+                alert("Date is Empty!!!");
+            }else if(pointsVal==""){
+                alert("Weight is EMPTY!!!");
+            }else{
+            let arr = [dataVal,pointsVal];
+            historyData[hists]=arr;
+            for(let i=0;i<historyData.length;i++){
+                console.log(historyData[i]);
+            }
+            hists++;
+            document.getElementById("text1").value = "";
+            document.getElementById("text2").value = "";
+        }
+        });
         trashVisible=true;
         redeemVisible=false;
         historyVisible=false;
@@ -30,9 +83,12 @@ redeem.addEventListener("click",function(){
 });
 history.addEventListener("click",function(){
     if(historyVisible==false){
-        let modal = `<div class="modal">
-        <h1>hellhis</h1>
-    </div>`
+         let modal = "<ul>";
+        for (let i = 0; i < historyData.length; i++) {
+            console.log(historyData[i][0]);
+        modal += "<li>" + historyData[i][0] + "      " + historyData[i][1] + "</li>";
+        }
+        modal += "</ul>";
         MC.innerHTML=modal;
         trashVisible=false;
         redeemVisible=false;
